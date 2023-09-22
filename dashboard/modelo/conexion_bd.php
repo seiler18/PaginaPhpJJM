0e48d02ba3baae3c06dcc42778a0afcaa3ee3153
Jesus Seiler <ichbinseiler@gmail.com>
2023-09-21 23:02:41 -0300

    probando conexion a la base de datos

--- dashboard/modelo/conexion_bd.php
+++ dashboard/modelo/conexion_bd.php
@@ -1,4 +1,13 @@
 <?php
 $conexion = new mysqli("localhost", "root","", "prueba");
 $conexion->set_charset("utf8");
+// Verificar la conexión
+if ($conexion->connect_error) {
+    $respuesta = array("conectado" => false);
+} else {
+    $respuesta = array("conectado" => true);
+}
+
+header('Content-Type: application/json');
+echo json_encode($respuesta);
 ?>
