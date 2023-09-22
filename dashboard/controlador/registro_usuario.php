e0dbfebc9fa10f6cc1ba9b7b9b6a370b05f96b40
Jesus Seiler <ichbinseiler@gmail.com>
2023-09-21 22:06:18 -0300

    preparando entorno para php

---
+++ dashboard/controlador/registro_usuario.php
@@ -0,0 +1,18 @@
+<?php
+if (!empty($_POST["registro"])){
+if (empty($_POST["nombreUsuario"]) or empty($_POST["correo"]) or empty($_POST["contrasena"])) {
+
+echo 'Uno de los campos esta vacio'
+
+}   else {
+    
+}
+
+
+
+}
+
+
+
+
+?>
---
+++ dashboard/modelo/conexion_bd.php
@@ -0,0 +1,4 @@
+<?php
+$conexion = new mysqli("localhost", "root","", "prueba");
+$conexion->set_charset("utf8");
+?>
